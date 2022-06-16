c1ad7186d44aac58f7812f9f0a310ba869bdf93b
Alessandro <alessandropicchetti@hotmail.it>
2022-06-16 12:05:07 +0200

    refactor

--- Scripts/Game/Remake/ABL_AnimatedDecalManager.c
+++ Scripts/Game/Remake/ABL_AnimatedDecalManager.c
@@ -20,7 +20,7 @@ class ABL_AnimatedDecalManager : GenericEntity
 	
 	private World m_world;
 	int materialColor;	//todo make this dynamic 
-	const float WAIT_TIME = 0.2;		//500 ms
+	const float WAIT_TIME = 0.1;		//100 ms
 	float currentTime;
 
 	
@@ -45,40 +45,36 @@ class ABL_AnimatedDecalManager : GenericEntity
 		
 		Print("ADM: Starting up ADM");
 		decalsSpawned = new map<int, ref DecalInformation>();
-		
 		animationMaterials = new map<EDecalType, ref array<ResourceName>>();
-		animationMaterials.Insert(EDecalType.BLOODPOOL, {"{B1934F7967DAD951}bloodpool/materials/1.emat", "{B1934F7967DAD951}bloodpool/materials/1.emat","{EF7B663AD857575E}bloodpool/materials/2.emat",
-		"{1B7321A2D58A3F2A}bloodpool/materials/3.emat","{52AB34BDA74C4B40}bloodpool/materials/4.emat","{A6A37325AA912334}bloodpool/materials/5.emat","{F84B5A66151CAD3B}bloodpool/materials/6.emat",
-		"{0C431DFE18C1C54F}bloodpool/materials/7.emat","{6BFB7058F09045EF}bloodpool/materials/8.emat","{9FF337C0FD4D2D9B}bloodpool/materials/9.emat","{48FB2AAC7FBE89B1}bloodpool/materials/10.emat",
-		"{BCF36D347263E1C5}bloodpool/materials/11.emat","{E21B4477CDEE6FCA}bloodpool/materials/12.emat","{161303EFC03307BE}bloodpool/materials/13.emat","{5FCB16F0B2F573D4}bloodpool/materials/14.emat",
-		"{ABC35168BF281BA0}bloodpool/materials/15.emat","{F52B782B00A595AF}bloodpool/materials/16.emat","{01233FB30D78FDDB}bloodpool/materials/17.emat","{669B5215E5297D7B}bloodpool/materials/18.emat",
-		"{9293158DE8F4150F}bloodpool/materials/19.emat","{425E7AE1C9AE7A36}bloodpool/materials/20.emat","{B6563D79C4731242}bloodpool/materials/21.emat","{E8BE143A7BFE9C4D}bloodpool/materials/22.emat",
-		"{1CB653A27623F439}bloodpool/materials/23.emat","{556E46BD04E58053}bloodpool/materials/24.emat","{A16601250938E827}bloodpool/materials/25.emat","{FF8E2866B6B56628}bloodpool/materials/26.emat",
-		"{0B866FFEBB680E5C}bloodpool/materials/27.emat","{6C3E025853398EFC}bloodpool/materials/28.emat","{983645C05EE4E688}bloodpool/materials/29.emat","{8592EA7C3CF8393A}bloodpool/materials/30.emat",
-		"{719AADE43125514E}bloodpool/materials/31.emat","{2F7284A78EA8DF41}bloodpool/materials/32.emat","{DB7AC33F8375B735}bloodpool/materials/33.emat","{92A2D620F1B3C35F}bloodpool/materials/34.emat",
-		"{66AA91B8FC6EAB2B}bloodpool/materials/35.emat","{3842B8FB43E32524}bloodpool/materials/36.emat","{CC4AFF634E3E4D50}bloodpool/materials/37.emat","{ABF292C5A66FCDF0}bloodpool/materials/38.emat",
-		"{5FFAD55DABB2A584}bloodpool/materials/39.emat","{5714DA7AA58F9D38}bloodpool/materials/40.emat","{A31C9DE2A852F54C}bloodpool/materials/41.emat","{FDF4B4A117DF7B43}bloodpool/materials/42.emat",
-		"{09FCF3391A021337}bloodpool/materials/43.emat","{4024E62668C4675D}bloodpool/materials/44.emat","{B42CA1BE65190F29}bloodpool/materials/45.emat","{EAC488FDDA948126}bloodpool/materials/46.emat",
-		"{1ECCCF65D749E952}bloodpool/materials/47.emat","{7974A2C33F1869F2}bloodpool/materials/48.emat","{8D7CE55B32C50186}bloodpool/materials/49.emat"});
+		animationMaterials.Insert(EDecalType.BLOODPOOL, {"{B1934F7967DAD951}materials/bloodpool/1.emat","{502E981AD9811291}materials/bloodpool/2.emat","{A426DF82D45C7AE5}materials/bloodpool/3.emat",
+			"{EDFECA9DA69A0E8F}materials/bloodpool/4.emat","{19F68D05AB4766FB}materials/bloodpool/5.emat","{471EA44614CAE8F4}materials/bloodpool/6.emat","{B316E3DE19178080}materials/bloodpool/7.emat",
+			"{D4AE8E78F1460020}materials/bloodpool/8.emat","{20A6C9E0FC9B6854}materials/bloodpool/9.emat","{5DFCEE9E8D12D299}materials/bloodpool/10.emat","{A9F4A90680CFBAED}materials/bloodpool/11.emat",
+			"{F71C80453F4234E2}materials/bloodpool/12.emat","{0314C7DD329F5C96}materials/bloodpool/13.emat","{4ACCD2C2405928FC}materials/bloodpool/14.emat","{BEC4955A4D844088}materials/bloodpool/15.emat",
+			"{E02CBC19F209CE87}materials/bloodpool/16.emat","{1424FB81FFD4A6F3}materials/bloodpool/17.emat","{739C962717852653}materials/bloodpool/18.emat","{8794D1BF1A584E27}materials/bloodpool/19.emat",
+			"{5759BED33B02211E}materials/bloodpool/20.emat","{A351F94B36DF496A}materials/bloodpool/21.emat","{FDB9D0088952C765}materials/bloodpool/22.emat","{09B19790848FAF11}materials/bloodpool/23.emat",
+			"{4069828FF649DB7B}materials/bloodpool/24.emat","{B461C517FB94B30F}materials/bloodpool/25.emat","{EA89EC5444193D00}materials/bloodpool/26.emat","{1E81ABCC49C45574}materials/bloodpool/27.emat",
+			"{7939C66AA195D5D4}materials/bloodpool/28.emat","{8D3181F2AC48BDA0}materials/bloodpool/29.emat","{90952E4ECE546212}materials/bloodpool/30.emat","{649D69D6C3890A66}materials/bloodpool/31.emat",
+			"{3A7540957C048469}materials/bloodpool/32.emat","{CE7D070D71D9EC1D}materials/bloodpool/33.emat","{87A51212031F9877}materials/bloodpool/34.emat","{73AD558A0EC2F003}materials/bloodpool/35.emat",
+			"{2D457CC9B14F7E0C}materials/bloodpool/36.emat","{D94D3B51BC921678}materials/bloodpool/37.emat","{BEF556F754C396D8}materials/bloodpool/38.emat","{4AFD116F591EFEAC}materials/bloodpool/39.emat",
+			"{42131E485723C610}materials/bloodpool/40.emat","{B61B59D05AFEAE64}materials/bloodpool/41.emat","{E8F37093E573206B}materials/bloodpool/42.emat","{1CFB370BE8AE481F}materials/bloodpool/43.emat",
+			"{552322149A683C75}materials/bloodpool/44.emat","{A12B658C97B55401}materials/bloodpool/45.emat","{FFC34CCF2838DA0E}materials/bloodpool/46.emat","{0BCB0B5725E5B27A}materials/bloodpool/47.emat",
+			"{6C7366F1CDB432DA}materials/bloodpool/48.emat","{987B2169C0695AAE}materials/bloodpool/49.emat"});
 		
-		
-		animationMaterials.Insert(EDecalType.GENERIC_SPLATTER, {"{C7ADE32E13BAE5F3}test3/materials/0.emat","{1FFD03D5B7529B65}test3/materials/1.emat","{41152A9608DF156A}test3/materials/2.emat",
-		"{B51D6D0E05027D1E}test3/materials/3.emat","{FCC5781177C40974}test3/materials/4.emat","{08CD3F897A196100}test3/materials/5.emat","{562516CAC594EF0F}test3/materials/6.emat",
-		"{A22D5152C849877B}test3/materials/7.emat","{C5953CF4201807DB}test3/materials/8.emat","{319D7B6C2DC56FAF}test3/materials/9.emat","{4283FB5BEA1E9AE5}test3/materials/10.emat",
-		"{B68BBCC3E7C3F291}test3/materials/11.emat","{E8639580584E7C9E}test3/materials/12.emat","{1C6BD218559314EA}test3/materials/13.emat","{55B3C70727556080}test3/materials/14.emat",
-		"{A1BB809F2A8808F4}test3/materials/15.emat","{FF53A9DC950586FB}test3/materials/16.emat","{0B5BEE4498D8EE8F}test3/materials/17.emat","{6CE383E270896E2F}test3/materials/18.emat",
-		"{98EBC47A7D54065B}test3/materials/19.emat","{4826AB165C0E6962}test3/materials/20.emat","{BC2EEC8E51D30116}test3/materials/21.emat","{E2C6C5CDEE5E8F19}test3/materials/22.emat",
-		"{16CE8255E383E76D}test3/materials/23.emat","{5F16974A91459307}test3/materials/24.emat","{AB1ED0D29C98FB73}test3/materials/25.emat","{F5F6F9912315757C}test3/materials/26.emat",
-		"{01FEBE092EC81D08}test3/materials/27.emat","{6646D3AFC6999DA8}test3/materials/28.emat","{924E9437CB44F5DC}test3/materials/29.emat","{8FEA3B8BA9582A6E}test3/materials/30.emat",
-		"{7BE27C13A485421A}test3/materials/31.emat","{250A55501B08CC15}test3/materials/32.emat","{D10212C816D5A461}test3/materials/33.emat","{98DA07D76413D00B}test3/materials/34.emat",
-		"{6CD2404F69CEB87F}test3/materials/35.emat","{323A690CD6433670}test3/materials/36.emat","{C6322E94DB9E5E04}test3/materials/37.emat","{A18A433233CFDEA4}test3/materials/38.emat",
-		"{558204AA3E12B6D0}test3/materials/39.emat","{5D6C0B8D302F8E6C}test3/materials/40.emat","{A9644C153DF2E618}test3/materials/41.emat","{F78C6556827F6817}test3/materials/42.emat",
-		"{038422CE8FA20063}test3/materials/43.emat","{4A5C37D1FD647409}test3/materials/44.emat","{BE547049F0B91C7D}test3/materials/45.emat","{E0BC590A4F349272}test3/materials/46.emat"
-		,"{14B41E9242E9FA06}test3/materials/47.emat","{730C7334AAB87AA6}test3/materials/48.emat","{870434ACA76512D2}test3/materials/49.emat","{9AA09B10C579CD60}test3/materials/50.emat",
-		"{6EA8DC88C8A4A514}test3/materials/51.emat","{3040F5CB77292B1B}test3/materials/52.emat","{C448B2537AF4436F}test3/materials/53.emat","{8D90A74C08323705}test3/materials/54.emat",
-		"{7998E0D405EF5F71}test3/materials/55.emat","{2770C997BA62D17E}test3/materials/56.emat","{D3788E0FB7BFB90A}test3/materials/57.emat","{B4C0E3A95FEE39AA}test3/materials/58.emat",
-		"{40C8A431523351DE}test3/materials/59.emat","{9005CB5D73693EE7}test3/materials/60.emat","{640D8CC57EB45693}test3/materials/61.emat","{3AE5A586C139D89C}test3/materials/62.emat",
-		"{CEEDE21ECCE4B0E8}test3/materials/63.emat"});
+
+		animationMaterials.Insert(EDecalType.GENERIC_SPLATTER, {"{9BF28FFBA19F287D}materials/wall_splatter/0.emat","{1FFD03D5B7529B65}materials/wall_splatter/1.emat","{3112E12013CFCE06}materials/wall_splatter/2.emat",
+			"{C51AA6B81E12A672}materials/wall_splatter/3.emat","{8CC2B3A76CD4D218}materials/wall_splatter/4.emat","{78CAF43F6109BA6C}materials/wall_splatter/5.emat","{2622DD7CDE843463}materials/wall_splatter/6.emat","{D22A9AE4D3595C17}materials/wall_splatter/7.emat",
+			"{B592F7423B08DCB7}materials/wall_splatter/8.emat","{419AB0DA36D5B4C3}materials/wall_splatter/9.emat","{350AC655A18B59FB}materials/wall_splatter/10.emat","{C10281CDAC56318F}materials/wall_splatter/11.emat","{9FEAA88E13DBBF80}materials/wall_splatter/12.emat",
+			"{6BE2EF161E06D7F4}materials/wall_splatter/13.emat","{223AFA096CC0A39E}materials/wall_splatter/14.emat","{D632BD91611DCBEA}materials/wall_splatter/15.emat","{88DA94D2DE9045E5}materials/wall_splatter/16.emat","{7CD2D34AD34D2D91}materials/wall_splatter/17.emat",
+			"{1B6ABEEC3B1CAD31}materials/wall_splatter/18.emat","{EF62F97436C1C545}materials/wall_splatter/19.emat","{3FAF9618179BAA7C}materials/wall_splatter/20.emat","{CBA7D1801A46C208}materials/wall_splatter/21.emat","{954FF8C3A5CB4C07}materials/wall_splatter/22.emat",
+			"{6147BF5BA8162473}materials/wall_splatter/23.emat","{289FAA44DAD05019}materials/wall_splatter/24.emat","{DC97EDDCD70D386D}materials/wall_splatter/25.emat","{827FC49F6880B662}materials/wall_splatter/26.emat","{76778307655DDE16}materials/wall_splatter/27.emat",
+			"{11CFEEA18D0C5EB6}materials/wall_splatter/28.emat","{E5C7A93980D136C2}materials/wall_splatter/29.emat","{F8630685E2CDE970}materials/wall_splatter/30.emat","{0C6B411DEF108104}materials/wall_splatter/31.emat","{5283685E509D0F0B}materials/wall_splatter/32.emat",
+			"{A68B2FC65D40677F}materials/wall_splatter/33.emat","{EF533AD92F861315}materials/wall_splatter/34.emat","{1B5B7D41225B7B61}materials/wall_splatter/35.emat","{45B354029DD6F56E}materials/wall_splatter/36.emat","{B1BB139A900B9D1A}materials/wall_splatter/37.emat",
+			"{D6037E3C785A1DBA}materials/wall_splatter/38.emat","{220B39A4758775CE}materials/wall_splatter/39.emat","{2AE536837BBA4D72}materials/wall_splatter/40.emat","{DEED711B76672506}materials/wall_splatter/41.emat","{80055858C9EAAB09}materials/wall_splatter/42.emat",
+			"{740D1FC0C437C37D}materials/wall_splatter/43.emat","{3DD50ADFB6F1B717}materials/wall_splatter/44.emat","{C9DD4D47BB2CDF63}materials/wall_splatter/45.emat","{9735640404A1516C}materials/wall_splatter/46.emat","{633D239C097C3918}materials/wall_splatter/47.emat",
+			"{04854E3AE12DB9B8}materials/wall_splatter/48.emat","{F08D09A2ECF0D1CC}materials/wall_splatter/49.emat","{ED29A61E8EEC0E7E}materials/wall_splatter/50.emat","{1921E1868331660A}materials/wall_splatter/51.emat","{47C9C8C53CBCE805}materials/wall_splatter/52.emat",
+			"{B3C18F5D31618071}materials/wall_splatter/53.emat","{FA199A4243A7F41B}materials/wall_splatter/54.emat","{0E11DDDA4E7A9C6F}materials/wall_splatter/55.emat","{50F9F499F1F71260}materials/wall_splatter/56.emat","{A4F1B301FC2A7A14}materials/wall_splatter/57.emat",
+			"{C349DEA7147BFAB4}materials/wall_splatter/58.emat","{3741993F19A692C0}materials/wall_splatter/59.emat","{E78CF65338FCFDF9}materials/wall_splatter/60.emat","{1384B1CB3521958D}materials/wall_splatter/61.emat","{4D6C98888AAC1B82}materials/wall_splatter/62.emat",
+			"{B964DF10877173F6}materials/wall_splatter/63.emat"});
 		
 		
 		instance = this;
