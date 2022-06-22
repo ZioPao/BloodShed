10c438a2d637d0670dc2f3ca274479c486ca5eca
Alessandro <alessandropicchetti@hotmail.it>
2022-06-22 20:13:55 +0200

    fixed bloodpool interpolation

--- Scripts/Game/Remake/ABL_CharacterDamageManagerComponent.c
+++ Scripts/Game/Remake/ABL_CharacterDamageManagerComponent.c
@@ -9,12 +9,9 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 	bool alreadyDestroyed = false;
 	World worldTmp;
 
-	const int MAX_DECALS_PER_CHAR = 2;
-	const int MAX_DECALS_PLAYER_WEAPON = 6;
 	
 	ref static array<ref DecalWrapper> currentPlayerDecals;
 	ref array<ref DecalWrapper> currentCharacterDecals;
-	ref static array<ref Shape> dbgShapes;
 
 	override void OnInit(IEntity owner)
 	{
@@ -41,6 +38,10 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 		ablVariablesMap.Set("decalAngle", new VariableInfo("Decal Angle", "0"));
 		ablVariablesMap.Set("nearClip", new VariableInfo("Near Clip (TEST)", "0"));
 		ablVariablesMap.Set("farClip", new VariableInfo("Far Clip (TEST)", "5"));
+		
+		
+		ablVariablesMap.Set("maxDecalsPerChar", new VariableInfo("Max Decals per Character", "2"));
+		ablVariablesMap.Set("maxDecalsPlayerWeapon", new VariableInfo("Max Decals for Player Weapon", "6"));
 		ablVariablesMap.Set("debugSpheres", new VariableInfo("Debug Spheres", "0"));
 	
 		if (!ABL_mcfSettingsManager.GetJsonManager(ABL_MOD_ID))
@@ -56,7 +57,6 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 		//////////////////////////////////////////////////////////////////////////////////////////////////
 		
 		
-		dbgShapes = new array<ref Shape>();
 	}
 
 	override void OnDamage(
@@ -76,7 +76,7 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 		
 		
 		
-		ablSettings = MCF_SettingsManager.GetInstance().GetModSettings("59951797A291CA02");
+		ablSettings = MCF_SettingsManager.GetInstance().GetModSettings(ABL_MOD_ID);
 
 
 		array<string> boneNames = new array<string>();
@@ -118,7 +118,9 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 
 			currentPlayer.GetTransform(playerTransform);
 			//Print("Applying Decal on damaged entity");
-			ManageDecalStack(currentCharacterDecals, ownerChar, hitTransform[0], hitTransform[1], 0, 2, MAX_DECALS_PER_CHAR);
+			
+			int maxDecalsPerChar = ablSettings.Get("maxDecalsPerChar").ToInt();
+			ManageDecalStack(currentCharacterDecals, ownerChar, hitTransform[0], hitTransform[1], 0, 2, maxDecalsPerChar);
 
 			vector originChar = ownerChar.GetOrigin();
 			float farClip = ablSettings.Get("farClip").ToFloat();
@@ -126,8 +128,10 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 			if (vector.Distance(playerTransform[3], originChar) < farClip)
 			{
 				//Print("Applying Decals on Player Weapon");
+				int maxDecalsPlayerWeapon = ablSettings.Get("maxDecalsPlayerWeapon").ToInt();
+
 				float nearClip = ablSettings.Get("nearClip").ToFloat();
-				ManageDecalStack(currentPlayerDecals, currentPlayer, hitTransform[0], hitTransform[1], nearClip, farClip,MAX_DECALS_PLAYER_WEAPON);
+				ManageDecalStack(currentPlayerDecals, currentPlayer, hitTransform[0], hitTransform[1], nearClip, farClip, maxDecalsPlayerWeapon);
 				Print("________________________");
 			}
 		}
@@ -148,8 +152,7 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 		float intersectionDistance = worldTmp.TraceMove(param, NULL) * farClip;
 		vector intersectionPosition = hitPosition + (hitDirection * intersectionDistance);
 		
-		//set dynamic stuff here 
-		ablSettings = MCF_SettingsManager.GetInstance().GetModSettings("59951797A291CA02");
+		ablSettings = MCF_SettingsManager.GetInstance().GetModSettings(ABL_MOD_ID);
 		float diffOriginX = ablSettings.Get("diffOriginX").ToFloat();
 		float diffOriginY = ablSettings.Get("diffOriginY").ToFloat();
 		float diffOriginZ = ablSettings.Get("diffOriginZ").ToFloat();
@@ -162,7 +165,7 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 		float rotation = Math.RandomFloatInclusive(-decalAngle, decalAngle);
 
 		//ResourceName tempMaterial = "{82129FA9BA80D8F4}materials/WeaponBloodSecondTry.emat";
-		ResourceName tempMaterial = "{098960A4823D679F}materials/WeaponBlood.emat";
+		ResourceName tempMaterial = "{098960A4823D679F}materials/weapon_splatter/WeaponBlood.emat";
 		
 		int count = stack.Count();
 		if (count >= maxDecals)
@@ -200,7 +203,7 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 			DecalWrapper newTmpWrapperLeft = DecalWrapper(worldTmp.CreateDecal(owner, tmpOrigin, tmpProjection, nearClip, farClip, rotation, 1, 1, tempMaterial, -1, materialColor));
 			stack.Insert(newTmpWrapperLeft);
 			if(debugSpheres)
-				Debug_DrawSphereAtPos(tmpOrigin, COLOR_RED);
+				MCF_Debug.DrawSphereAtPos(tmpOrigin, COLOR_RED);
 
 		}
 
@@ -209,18 +212,7 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 	
 	
 	//------------------------------------------------------------------------------------------------
-	private void Debug_DrawSphereAtPos(vector pos, int color)
-	{
-		vector matx[4];
-		Math3D.MatrixIdentity4(matx);
-		matx[3] = pos;
-		int shapeFlags = ShapeFlags.DEFAULT;
-		Shape s = Shape.CreateSphere(color, shapeFlags, pos, 0.05);
-		s.SetMatrix(matx);
-		dbgShapes.Insert(s);
-	}
-	
-	
+
 	
 	static void CleanWeapon()
 	{
@@ -315,81 +307,6 @@ class TestClass
 
 	//delete it... jesus what a shitshow
 }
-
-
-
-
-class Queue<Class T>
-{
-	protected ref array<ref T> m_aArray;
-	
-	//-----------------------------------------------------------------------------------------------------------
-	//! Push an item to the end of the stack
-	void Push(T item) 
-	{
-		m_aArray.Insert(item);
-		
-		
-		int length = m_aArray.Count();
-		
-		if (length >= 1)
-		{
-		
-		
-			
-		}
-	}
-	
-	//-----------------------------------------------------------------------------------------------------------
-	//! Pop an item from the end of the stack (or null if none)
-	T Pop()
-	{
-		int length = m_aArray.Count();
-		if (length >= 1)
-		{
-			int index = length-1;
-			
-			ref T poppedItem = m_aArray[index];
-			m_aArray.Remove(index);
-			
-			return poppedItem;
-		}
-		
-		return null;
-	}
-	
-	//-----------------------------------------------------------------------------------------------------------
-	//! Returns true if stack is empty.
-	bool IsEmpty()
-	{
-		return (m_aArray.Count() == 0);
-	}
-	
-	//-----------------------------------------------------------------------------------------------------------
-	//! Returns the count of elements stored in this stack.
-	int Count()
-	{
-		return m_aArray.Count();
-	}
-	
-	//-----------------------------------------------------------------------------------------------------------
-	//! Create an empty stack, initialize variables
-	void SCR_Stack()
-	{
-		m_aArray = new ref array<ref T>();
-	}
-	
-	//-----------------------------------------------------------------------------------------------------------
-	//! Cleanup, release variables
-	void ~SCR_Stack()
-	{
-		if (m_aArray)
-		{
-			m_aArray.Clear();
-			m_aArray = null;			
-		}
-	}
-};
 
 
 
